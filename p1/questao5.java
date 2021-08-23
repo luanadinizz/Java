3f7acb56f002c666f9df283dd528d7e85ce565be
luanadinizz <89155982+luanadinizz@users.noreply.github.com>
2021-08-23 16:48:26 -0300

    Create questao5.java

---
+++ p1/questao5.java
@@ -0,0 +1,10 @@
+class exercicio5{
+  public static void main ( String[]args ){
+      String nome = "Luana";
+      int N = nome.length();
+      for (int i=1;i<=N;i++){
+          System.out.println("Luana");
+      }
+  
+  }
+ 
