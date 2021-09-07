ae138c6a6abacdfbee1489b5570ed9f5364c4bc5
Luana Diniz <luanadiniz@id.uff.br>
2021-09-07 17:08:59 -0300

    Create Contato.java

---
+++ p4/agenda/questao2/model/Contato.java
@@ -0,0 +1,44 @@
+package br.uff.ic.agenda.model;
+
+import java.io.Serializable;
+
+public class Contato implements Serializable {
+    private String nome;
+    private String telefone;
+    private String detalhes;
+
+    public Contato() {
+        nome = "Novo Contato";
+        telefone = "";
+        detalhes = "";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDetalhes() {
+        return detalhes;
+    }
+
+    public void setDetalhes(String detalhes) {
+        this.detalhes = detalhes;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }    
+}
