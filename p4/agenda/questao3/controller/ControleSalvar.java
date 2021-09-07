794e67c6f9f780ebbf7655a3f30b053a6d01cac0
Luana Diniz <luanadiniz@id.uff.br>
2021-09-07 17:13:12 -0300

    Create ControleSalvar.java

---
+++ p4/agenda/questao3/controller/ControleSalvar.java
@@ -0,0 +1,34 @@
+package br.uff.ic.agenda.controller;
+
+import br.uff.ic.agenda.model.Contato;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JList;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class ControleSalvar extends KeyAdapter {
+
+    private final JList<Contato> listaContatos;
+    private final JTextField campoNome;
+    private final JTextField campoTelefone;
+    private final JTextArea campoDetalhes;
+    
+    public ControleSalvar (JList<Contato> listaContatos, JTextField campoNome, JTextField campoTelefone, JTextArea campoDetalhes) {
+        this.listaContatos = listaContatos;
+        this.campoNome = campoNome;
+        this.campoTelefone = campoTelefone;
+        this.campoDetalhes = campoDetalhes;
+    }
+    
+    @Override
+    public void keyReleased(KeyEvent e) {
+        Contato contatoSelecionado = listaContatos.getSelectedValue();
+        if (contatoSelecionado != null) {
+            contatoSelecionado.setNome(campoNome.getText());
+            contatoSelecionado.setTelefone(campoTelefone.getText());
+            contatoSelecionado.setDetalhes(campoDetalhes.getText());
+        }
+        listaContatos.repaint();        
+    }    
+}
