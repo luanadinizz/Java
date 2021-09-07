5ae6ad442c489e2d18719ad5652748a940343829
Luana Diniz <luanadiniz@id.uff.br>
2021-09-07 16:59:02 -0300

    Create ControleAdicionar.java

---
+++ p4/agenda/controller/ControleAdicionar.java
@@ -0,0 +1,20 @@
+package br.uff.ic.agenda.controller;
+
+import br.uff.ic.agenda.model.Contato;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.DefaultListModel;
+
+public class ControleAdicionar implements ActionListener {
+
+    private final DefaultListModel<Contato> contatos;
+    
+    public ControleAdicionar (DefaultListModel<Contato> contatos) {
+        this.contatos = contatos;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        contatos.addElement(new Contato());
+    }
+}
